d7a56f85aa3a9427e96c66879dc41e47d11207fb
Samuel Fosu <sfosu003@gmail.com>
2022-12-09 21:04:35 -0800

    Smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
- * Return: 0
+ * Return: Always 0 (success)
  */
+
 int main(void)
 {
 	char c;
@@ -15,6 +17,6 @@ int main(void)
 			putchar(c);
 			c--;
 		}
-	putchar('\n);
+	putchar('\n');
 	return (0);
 }
