de547cfe8a19a184551428ba80dd789d696d4404
Samuel Fosu <sfosu003@gmail.com>
2022-12-09 19:18:39 -0800

    I sometimes suffer from insomniagit push

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 
 /**
- *
- * * main - prints the alphabet in lowercase
- *
- * * Return: 0
- *
- * */
+ * main - prints the alphabet in lowercase
+ * Return: 0
+ */
 
 int main(void)
 {
 	char ch;
-	
+
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar(ch);
