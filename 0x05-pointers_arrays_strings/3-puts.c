7a6b324225ea3819ea00eb7130b66c33dd916c44
Samuel Fosu <sfosu003@gmail.com>
2022-12-19 15:02:36 -0800

    Discription for puts added

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _putchar - prints a string, followed by a new line.
+ * _puts - prints a string, followed by a new line.
  * @str: input string.
  * Return: no return.
  */
